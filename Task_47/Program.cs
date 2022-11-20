3e975e36796f1c4bf924ea547399fff71d7151b4
Ilya12184 <volostnov.il@yandex.ru>
2022-11-20 11:26:46 +0300

    Добавлена задача 47

---
+++ Task_47/Program.cs
@@ -0,0 +1,31 @@
+﻿/*Задача 47. Задайте двумерный массив размером m×n, заполненный случайными 
+вещественными числами.*/
+
+double[,] matrix = new double[3, 4];
+
+void PrintArray(double[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray(double[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i,j] = Convert.ToDouble(new Random().NextDouble()*100);
+        }    
+    }
+}
+
+double[,] matr = new double[3, 4];
+FillArray(matrix);
+PrintArray(matrix);
